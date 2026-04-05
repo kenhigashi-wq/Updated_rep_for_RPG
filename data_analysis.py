275b094cf2e76302a77d2dfde41888b0610993ea
kenny <ken.higashi@ucas-edu.net>
2026-04-05 22:23:54 +0000

    h

--- data_analysis.py
+++ data_analysis.py
@@ -1,33 +1,18 @@
-#KH data stuff with panda, UPDATED
-
 import pandas as pd
 
-def build_character_dataframe(stats_dict):
+class StatisticalAnalyzer:
+    def __init__(self, stats_dict):
+        self.df = pd.DataFrame.from_dict(stats_dict, orient="index")
 
-    df = pd.DataFrame.from_dict(stats_dict, orient="index")
-    df.index.name = "Character"
-    return df
+    def basic_analysis(self):
+        return {
+            "mean": self.df.mean(),
+            "min": self.df.min(),
+            "max": self.df.max()
+        }
 
+    def export_csv(self, filename="character_data.csv"):
+        self.df.to_csv(filename)
 
-def analyze_stats(df):
-
-    analysis = {
-        "mean": df.mean(),
-        "min": df.min(),
-        "max": df.max()
-    }
-    return analysis
-
-
-def export_to_csv(df, filename="character_stats.csv"):
-    df.to_csv(filename)
-    print(f"Character data exported to {filename}")
-
-
-def load_from_csv(filename="character_stats.csv"):
-    df = pd.read_csv(filename, index_col="Character")
-    print(f"Character data loaded from {filename}")
-    return df
-
-def import_csv(filename="character_data.csv"):
-    return pd.read_csv(filename, index_col=0)
+    def import_csv(self, filename="character_data.csv"):
+        return pd.read_csv(filename, index_col=0)
